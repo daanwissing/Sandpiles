23f892b3aaf99b3f2c8b006d682222256e4de6bc
Daan Wissing <daan.wissing@gmail.com>
2020-11-10 14:18:59 +0100

    Collapse!

--- Sandpiles.Calc.Test/SandPileGridTests.cs
+++ Sandpiles.Calc.Test/SandPileGridTests.cs
@@ -5,7 +5,7 @@ namespace Sandpiles.Calc.Test
     public class SandPileGridTest
     {
         [Test]
-        public void Test1()
+        public void WhenNewGrid_GridInitialized()
         {
             // Arrange
             var sandPileGrid = new SandPileGrid();
@@ -16,6 +16,59 @@ namespace Sandpiles.Calc.Test
             // Assert
             Assert.That(grid.Length, Is.EqualTo(3));
             Assert.That(grid[0].Length, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void GivenDimensions_WhenNewGrid_ThenGridHasGivenDimensions()
+        {
+            // Arrange
+            var sandPileGrid = new SandPileGrid(6,9);
+
+            // Act
+            var grid = sandPileGrid.Grid;
+
+            // Assert
+            Assert.That(grid.Length, Is.EqualTo(6));
+            Assert.That(grid[0].Length, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void GivenEmptyGrid_WhenCollapse_ThenGridIsSame()
+        {
+            // Arrange
+            var sandPileGrid = new SandPileGrid(6,9);
+            var copy = sandPileGrid.Grid;
+
+            // Act
+            sandPileGrid.Collapse();
+
+            // Assert
+            var result = sandPileGrid.Grid;
+            for(int i = 0; i < copy.Length; i++)
+            {
+                for (int j = 0; j < copy[i].Length; j++)
+                {
+                    Assert.That(copy[i][j], Is.EqualTo(result[i][j]));
+                }
+            }
+        }
+
+        [Test]
+        public void Given4SandInCell_WhenCollapse_ThenSandIsDistributed()
+        {
+                        // Arrange
+            var sandPileGrid = new SandPileGrid();
+            sandPileGrid.Grid[1][1] = 4;
+
+            // Act
+            sandPileGrid.Collapse();
+
+            // Assert
+            Assert.That(sandPileGrid.Grid[0][1], Is.EqualTo(1));
+            Assert.That(sandPileGrid.Grid[1][0], Is.EqualTo(1));
+            Assert.That(sandPileGrid.Grid[1][2], Is.EqualTo(1));
+            Assert.That(sandPileGrid.Grid[2][1], Is.EqualTo(1));
+            Assert.That(sandPileGrid.Grid[1][1], Is.EqualTo(0));
         }
     }
 }
--- Sandpiles.Calc/SandPileGrid.cs
+++ Sandpiles.Calc/SandPileGrid.cs
@@ -4,13 +4,32 @@ namespace Sandpiles.Calc
 {
     public class SandPileGrid
     {
-        public int[][] Grid { get; }
+        private int[][] _grid;
+        public readonly int Height;
+        public readonly int Width;
 
-        public SandPileGrid() : this(3,3)
+
+        public int[][] Grid
+        {
+            get
+            {
+                return _grid; // TODO: copy
+            }
+        }
+
+        public SandPileGrid() : this(3, 3)
         {
         }
 
         public SandPileGrid(int height, int width)
+        {
+            int[][] newGrid = InitializeGrid(height, width);
+            _grid = newGrid;
+            Height = height;
+            Width = width;
+        }
+
+        private static int[][] InitializeGrid(int height, int width)
         {
             int[][] newGrid = new int[height][];
             for (int i = 0; i < height; i++)
@@ -18,12 +37,34 @@ namespace Sandpiles.Calc
                 var row = new int[width];
                 newGrid[i] = row;
             }
-            Grid = newGrid;
+
+            return newGrid;
         }
 
         public void Collapse()
         {
+            var newGrid = InitializeGrid(Height, Width);
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    if (Grid[i][j] > 3)
+                    {
+                        newGrid[i-1][j]++;
+                        newGrid[i+1][j]++;
+                        newGrid[i][j+1]++;
+                        newGrid[i][j-1]++;
 
+                        newGrid[i][j] = Grid[i][j] - 4;
+                    }
+                    else
+                    {
+                        newGrid[i][j] += Grid[i][j];
+                    }
+                }
+            }
+            
+            _grid = newGrid;
         }
     }
 }
